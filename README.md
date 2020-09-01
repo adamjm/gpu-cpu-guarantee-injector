46c6f63888918171b30579bc30ad613e7da88566
Adam Makarucha <adamjm@au1.ibm.com>
2020-09-01 10:50:32 +1000

    Initial commit of the forked code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gpu-cpu-guarantee-injector
 An admission controller that guarantee's the number of CPUs and Memory per GPU requested using Kubernetes QoS. 
+
+This injector was forked initially from the great work done by [Statistics Canada](https://github.com/StatCan/gpu-toleration-injector)
---
+++ mutate.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"k8s.io/api/admission/v1beta1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func mutate(request v1beta1.AdmissionRequest) (v1beta1.AdmissionResponse, error) {
+	response := v1beta1.AdmissionResponse{}
+
+	// Default response
+	response.Allowed = true
+	response.UID = request.UID
+
+	// Decode the pod object
+	var err error
+	pod := v1.Pod{}
+	if err := json.Unmarshal(request.Object.Raw, &pod); err != nil {
+		return response, fmt.Errorf("unable to decode Pod %w", err)
+	}
+
+	log.Printf("Check pod for GPU request %s/%s", pod.Namespace, pod.Name)
+
+	// Check for a GPU
+	hasGPU := false
+	for _, container := range pod.Spec.Containers {
+		// if container.Resources.Requests.
+		if limit, ok := container.Resources.Requests["nvidia.com/gpu"]; ok {
+			if !limit.IsZero() {
+				hasGPU = true
+				gpuCount = limit
+				break
+			}
+		}
+	}
+
+	if hasGPU {
+		log.Printf("Found GPU request for %s/%s", pod.Namespace, pod.Name)
+
+		patch := v1beta1.PatchTypeJSONPatch
+		response.PatchType = &patch
+
+		response.AuditAnnotations = map[string]string{
+			"cpu-guarentee-admission-controller": "Added dedicated=CPU Count",
+		}
+
+		//add resouces need functions for limits and requests 
+
+		patches := []map[string]interface{}{
+			{
+				"op":    "add",
+				"path":  "/spec/tolerations/-",
+				"value": toleration,
+			},
+		}
+		response.Patch, err = json.Marshal(patches)
+		if err != nil {
+			return response, err
+		}
+
+		response.Result = &metav1.Status{
+			Status: metav1.StatusSuccess,
+		}
+	}
+
+	return response, nil
+}
